65fdf40908343f2099d262171b5eca25e7517367
decodeais <jenspeterschrade@gmail.com>
2023-05-06 22:11:17 +0200

    Eergy counter

--- windTurbine.cpp
+++ windTurbine.cpp
@@ -6,16 +6,22 @@
 #include <windTurbine.h>
 #include <esp32-hal-adc.h>
 #include <remotedebug.h>
+#include <exteeprom.h>
+#include  "eeprom.h"
+
 
 #define I_SCALE_5A 2*5.4054054  /// A/V
 #define U_SCALE 0.0110294117647*1.082 // 75/6.8*x
-#define FILTER 0.3
+#define FILTER 0.2
 #define OFFS_FILTER 0.1
 
 //#define ERRORCONV 2
-
+ 
 AnaValueStruct AnaValue;
-
+float TempInt;
+float Wtemp;
+extern eepromStruct testdata;
+extern extEEPROM myEEPROM;
 /*Inputs*/
 bool ErrorConverter;
 bool calibration = false;
@@ -44,11 +50,20 @@ float Lowpass(float NewValue, float OldValue,float FilterKoeff)
   return OldValue*(1.0-FilterKoeff)+ NewValue*FilterKoeff;
 }
 
+void ResCount()
+{
+  testdata.Energie = 0;
+myEEPROM.write(offsetof(eepromStruct,Energie),(byte*) &(testdata.Energie), sizeof(testdata.Energie));
+myEEPROM.read(data(eepromStruct,testdata,Energie));
+ AnaValue.Wturb=	testdata.Energie;
+
+}
 
 void Measure_Analog()
 {
 static float ActFilter = 1.0;
 float I;
+float timePassed;
  static long int gnLastUpdate = millis();
     analogReadResolution(12);
     AnaValue.ch_0 = analogReadMilliVolts(36);
@@ -79,33 +94,64 @@ AnaValue.Ibatt = (AnaValue.ch_7*2.0/1000.0-AnaValue.Uvcc/2.0)*I_SCALE_5A;
 AnaValue.Iturb = (AnaValue.ch_4*2.0/1000.0-AnaValue.Uvcc/2.0)*I_SCALE_5A;
 */
 AnaValue.Ibatt = Lowpass((AnaValue.ch_7-AnaValue.ch_5/2.0)/1000*I_SCALE_5A, AnaValue.Ibatt, ActFilter) ; 
-AnaValue.Iturb = Lowpass((AnaValue.ch_4-AnaValue.ch_5/2.0)/1000*I_SCALE_5A, AnaValue.Iturb, ActFilter) ; 
 
-debugV("Ibatt : %f",AnaValue.Ibatt);
-debugV("Iturb : %f",AnaValue.Iturb);
+
+
 
 
 
 if ( SpecialSettings.TurbineSTOP == true) // calibration when turbine stopped
 {
-AnaValue.OffsIbatt = Lowpass(AnaValue.Ibatt,AnaValue.OffsIbatt, OFFS_FILTER); //calibration
+AnaValue.Iturb = Lowpass((AnaValue.ch_4-AnaValue.ch_5/2.0)/1000*I_SCALE_5A, AnaValue.Iturb, ActFilter/10) ; 
+//AnaValue.OffsIbatt = Lowpass(AnaValue.Ibatt,AnaValue.OffsIbatt, OFFS_FILTER); //calibration
 AnaValue.OffsIturb = Lowpass(AnaValue.Iturb,AnaValue.OffsIturb, OFFS_FILTER);
-if (abs(AnaValue.Iturb-AnaValue.OffsIturb) <= 0.01)
+if (abs(AnaValue.Iturb-AnaValue.OffsIturb) <= 0.005)
 {
   calibration = true;
 }
 }
+else
+{
+  AnaValue.Iturb = Lowpass((AnaValue.ch_4-AnaValue.ch_5/2.0)/1000*I_SCALE_5A, AnaValue.Iturb, ActFilter) ; 
 
+}
+debugV("Ibatt : %f",AnaValue.Ibatt);
+debugV("Iturb : %f",AnaValue.Iturb);
 AnaValue.Ubal = AnaValue.OffsIturb;
-//I=(AnaValue.Ibatt - AnaValue.Iturb)/2.0-0.22;I = -(AnaValue.Iturb - AnaValue.OffsIturb);
+//I=(AnaValue.Ibatt - AnaValue.Iturb)/2.0-0.22;
+debugV("Iturb : %f",AnaValue.Iturb);
+debugV("OffsIturb : %f",AnaValue.OffsIturb);
+I = -(AnaValue.Iturb - AnaValue.OffsIturb);
 if ( I < 0.0 ) 
 { 
   I = 0.0; 
 }
 AnaValue.Pturb = I*AnaValue.Uturb;
-AnaValue.Wturb = AnaValue.Pturb*timePassedSince(gnLastUpdate)/1000.0;
+debugV("I : %f",I);
+debugV("Uturb : %f",AnaValue.Uturb);
+timePassed = timePassedSince(gnLastUpdate);
+debugV("Pturb : %f",AnaValue.Pturb);
+Wtemp += AnaValue.Pturb*timePassed/1000.0;
+debugV("TimePassed : %f",timePassed);
+debugV("Wtemp : %f",Wtemp);
+//AnaValue.Wturb
+
+if  (Wtemp > 1000.0)
+{
+  Wtemp = modf (Wtemp , &TempInt); // fractpart = modf (param , &intpart);
+  debugV("Wtemp : %f",Wtemp);                                                                                                                      
+//AnaValue.Wturb += TempInt;
+testdata.Energie += TempInt;
+myEEPROM.write(offsetof(eepromStruct,Energie),(byte*) &(testdata.Energie), sizeof(testdata.Energie));
+}           
+AnaValue.Wturb=(testdata.Energie+Wtemp)/3600;
+debugV("Wturb : %f",AnaValue.Wturb);
+//testdata.Energie = AnaValue.Wturb;
+
+
+
 gnLastUpdate = millis();
-ActFilter = FILTER;
+ActFilter = FILTER;  // only ther first cycle is without Lowpass
 }
 
 
